af90a669be3af2f4480aca1d49f59cc02588697a
Polishchuk Arthur <arthurpolishchuk@outlook.com>
2022-01-25 18:17:05 +0200

    Добавьте файлы проекта.

---
+++ Microsoft-Defender-Control/DefenderController.cs
@@ -0,0 +1,105 @@
+﻿using System.Diagnostics;
+using Microsoft.Win32;
+
+namespace Microsoft_Defender_Control
+{
+    internal class DefenderController
+    {
+        private readonly RegistryKey _currentUserKey = Registry.LocalMachine;
+
+        public bool IsAutoRun
+        {
+            get
+            {
+                RegistryKey autoRunProgram = _currentUserKey.CreateSubKey(@"SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon");
+                return autoRunProgram.GetValue("Shell").ToString().Contains("Microsoft-Defender-Control.exe");
+            }
+        }
+
+        public void EnableDefender()
+        {
+            RegistryKey windowsDefenderKey = _currentUserKey.CreateSubKey(@"SOFTWARE\Policies\Microsoft\Windows Defender");
+            windowsDefenderKey.DeleteValue("AllowFastServiceStartup", false);
+            windowsDefenderKey.DeleteValue("DisableAntiSpyware", false);
+            windowsDefenderKey.DeleteValue("ServiceKeepAlive", false);
+
+            windowsDefenderKey = _currentUserKey.CreateSubKey(@"SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection");
+            windowsDefenderKey.DeleteValue("DisableIOAVProtection", false);
+            windowsDefenderKey.DeleteValue("DisableRealtimeMonitoring", false);
+
+            windowsDefenderKey = _currentUserKey.CreateSubKey(@"SOFTWARE\Policies\Microsoft\Windows Defender\Spynet");
+            windowsDefenderKey.DeleteValue("DisableBlockAtFirstSeen", false);
+            windowsDefenderKey.DeleteValue("LocalSettingOverrideSpynetReporting", false);
+            windowsDefenderKey.DeleteValue("SubmitSamplesConsent", false);
+
+            windowsDefenderKey = _currentUserKey.CreateSubKey(@"SYSTEM\CurrentControlSet\Services\WinDefend");
+            windowsDefenderKey.SetValue("Start", 2, RegistryValueKind.DWord);
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Now the pc will reboot!");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("Press to do this..");
+            Console.ResetColor();
+            Console.ReadKey();
+
+            var p = new Process();
+            p.StartInfo.FileName = @"C:\Windows\System32\cmd.exe";
+            p.StartInfo.WorkingDirectory = @"C:\Windows\System32";
+            p.StartInfo.Arguments = @"/k shutdown /r /t 5";
+            p.Start();
+        }
+
+        public void DisableDefender()
+        {
+            RegistryKey windowsDefenderKey = _currentUserKey.CreateSubKey(@"SOFTWARE\Policies\Microsoft\Windows Defender");
+            windowsDefenderKey.SetValue("DisableAntiSpyware", 1, RegistryValueKind.DWord);
+            windowsDefenderKey.SetValue("AllowFastServiceStartup", 0, RegistryValueKind.DWord);
+            windowsDefenderKey.SetValue("ServiceKeepAlive", 0, RegistryValueKind.DWord);
+
+            windowsDefenderKey = _currentUserKey.CreateSubKey(@"SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection");
+            windowsDefenderKey.SetValue("DisableIOAVProtection", 1, RegistryValueKind.DWord);
+            windowsDefenderKey.SetValue("DisableRealtimeMonitoring", 1, RegistryValueKind.DWord);
+
+            windowsDefenderKey = _currentUserKey.CreateSubKey(@"SOFTWARE\Policies\Microsoft\Windows Defender\Spynet");
+            windowsDefenderKey.SetValue("DisableBlockAtFirstSeen", 1, RegistryValueKind.DWord);
+            windowsDefenderKey.SetValue("LocalSettingOverrideSpynetReporting", 0, RegistryValueKind.DWord);
+            windowsDefenderKey.SetValue("SubmitSamplesConsent", 2, RegistryValueKind.DWord);
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Now the pc will reboot in safe mode!");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("Press to do it..");
+            Console.ReadKey();
+
+            var p = new Process();
+            p.StartInfo.FileName = @"C:\Windows\System32\cmd.exe";
+            p.StartInfo.WorkingDirectory = @"C:\Windows\System32";
+            p.StartInfo.Arguments = @"/k bcdedit /set {current} safeboot minimal";
+            p.Start();
+
+            p.StartInfo.Arguments = @"/k shutdown /r /t 5";
+            p.Start();
+
+            RegistryKey autoRunProgram = _currentUserKey.CreateSubKey(@"SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon");
+            autoRunProgram.SetValue("Shell", $@"explorer.exe, {new DirectoryInfo(@".").FullName}\Microsoft-Defender-Control.exe");
+        }
+
+        public void SafeModeStep()
+        {
+            RegistryKey windowsDefenderKey = _currentUserKey.CreateSubKey(@"SYSTEM\CurrentControlSet\Services\WinDefend");
+            windowsDefenderKey.SetValue("Start", 4, RegistryValueKind.DWord);
+
+            var p = new Process();
+            p.StartInfo.FileName = @"C:\Windows\System32\cmd.exe";
+            p.StartInfo.WorkingDirectory = @"C:\Windows\System32";
+            p.StartInfo.Arguments = @"/k bcdedit /deletevalue {current} safeboot";
+            p.Start();
+
+            p.StartInfo.Arguments = @"/k shutdown /r /t 0";
+            p.Start();
+
+            RegistryKey autoRunProgram = _currentUserKey.CreateSubKey(@"SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon");
+            autoRunProgram.SetValue("Shell", @"explorer.exe");
+        }
+    }
+}
---
+++ Microsoft-Defender-Control/Program.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft_Defender_Control;
+
+bool isExit = false;
+var defenderController = new DefenderController();
+
+if (defenderController.IsAutoRun)
+{
+    defenderController.SafeModeStep();
+    return;
+}
+
+do
+{
+    ShowMenu();
+} while (!isExit);
+
+void ShowMenu()
+{
+    Console.ForegroundColor = ConsoleColor.DarkGreen;
+    Console.WriteLine("1. Enable Defender");
+    Console.ForegroundColor = ConsoleColor.DarkRed;
+    Console.WriteLine("2. Disable Defender");
+    Console.ResetColor();
+    Console.Write("Enter (1-2): ");
+    string str = Console.ReadLine();
+
+    switch (str)
+    {
+        case "1":
+            defenderController.EnableDefender();
+            isExit = true;
+            return;
+        case "2":
+            defenderController.DisableDefender();
+            isExit = true;
+            return;
+        default:
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Beep();
+            Console.WriteLine("Incorrect choice! Enter correct choice (1-2). For example 1 or 2");
+            Console.ResetColor();
+            Thread.Sleep(1400);
+            Console.Clear();
+            break;
+    }
+}
